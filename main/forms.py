d8ff8f42b572bdcb50bb32ecbf90a231ae24578b
stauros <kroustou@gmail.com>
2012-05-19 17:08:37 +0300

    changing the layout a little bit :)

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 
 class NewUserForm(UserCreationForm):
-    first_name = forms.CharField(label=u'Όνομα')
-    last_name = forms.CharField(label=u'Επώνυμο')
-    is_staff = forms.BooleanField(required=False, label=u"Δάσκαλος", initial=False)
+    first_name = forms.CharField(label=u'Όνομα', help_text=u'το ονομά σας')
+    last_name = forms.CharField(label=u'Επώνυμο', help_text=u'το επωνυμό σας')
+    is_staff = forms.BooleanField(required=False, label=u"Δάσκαλος", initial=False, help_text=u'Είστε Δάσκαλος;')
 
