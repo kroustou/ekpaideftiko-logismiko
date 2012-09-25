b65ebd17b5074c89e9833a021afa99a8c467dd32
stauros <kroustou@gmail.com>
2012-09-26 01:14:56 +0300

    add generic relation to exercise

--- main/forms.py
+++ main/forms.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.forms import ModelForm
-from models import Example,Student, TrueOrFalse, FillTheBlanks, MultipleChoice, CHOICES, DIFFICULTIES, Test, Examination
+from models import Example,Student, TrueOrFalse, FillTheBlanks, MultipleChoice, CHOICES, DIFFICULTIES, Test, Examination, Chapter, Exercise
 
 EXERCISETYPES = (
         ('TrueOrFalse', 'Σωστό η Λάθος'),
@@ -10,8 +10,10 @@ EXERCISETYPES = (
         ('FillTheBlanks', 'συμπλήρωση κενού'),
     )
 
+
 class NewUserForm(ModelForm):
-    password = forms.CharField( widget=forms.PasswordInput, label="Κωδικός" )
+    password = forms.CharField(widget=forms.PasswordInput, label="Κωδικός")
+
     class Meta:
         model = Student
         fields = ('first_name', 'last_name', 'username', 'password')
@@ -19,28 +21,34 @@ class NewUserForm(ModelForm):
 
 class ExampleForm(ModelForm):
     form_type = forms.CharField(max_length=10, widget=forms.HiddenInput, initial='example')
+
     class Meta:
         model = Example
 
 #Exercise Creation Forms#
 #########################
+
+
 class TrueOrFalseForm(ModelForm):
     form_type = forms.CharField(max_length=10, widget=forms.HiddenInput, initial='ToF')
+
     class Meta:
         model = TrueOrFalse
 
-    
+
 class FillTheBlanksForm(ModelForm):
     form_type = forms.CharField(max_length=3, widget=forms.HiddenInput, initial='FtB')
+
     class Meta:
         model = FillTheBlanks
 
-    
+
 class MultipleChoiceForm(ModelForm):
     form_type = forms.CharField(max_length=2, widget=forms.HiddenInput, initial='MC')
+
     class Meta:
         model = MultipleChoice
-    
+
 ########################
 ########################
 
@@ -48,36 +56,46 @@ class MultipleChoiceForm(ModelForm):
 #Student Exercise Forms#
 ########################
 
+class ExerciseForm(forms.ModelForm):
+    exercise_type = forms.ChoiceField(label=u'Είδος άσκησης', choices=EXERCISETYPES)
+
+    class Meta:
+        model = Exercise
+        fields = ('chapterId', 'difficulty')
+
+
 class StudentTrueOrFalse(forms.Form):
     exercise_type = forms.CharField(max_length=10, widget=forms.HiddenInput, initial='ToF')
     answer = forms.ChoiceField(choices=CHOICES)
-    
-    
+
+
 class StudentFillTheBlanks(forms.Form):
     exercise_type = forms.CharField(max_length=3, widget=forms.HiddenInput, initial='FtB')
     answer = forms.CharField(max_length=255)
 
-    
+
 class StudentMultipleChoice(forms.Form):
     exercise_type = forms.CharField(max_length=2, widget=forms.HiddenInput, initial='MC')
     answer = forms.ChoiceField(choices=[1, 2, 3, 4])
-    
+
 ######################
 ######################
+
 
 class SelectLevelForm(forms.Form):
     level = forms.ChoiceField(label=u'Επίπεδο',
                                 choices=DIFFICULTIES)
 
+
+class SelectChapterForm(forms.Form):
+    chapter = forms.ModelChoiceField(queryset=Chapter.objects.all(), empty_label=None)
+
+
 class TestForm(ModelForm):
     class Meta:
         model = Test
 
+
 class ExamForm(ModelForm):
     class Meta:
         model = Examination
-
-class ExerciseForm(forms.Form):
-    exercise_type = forms.ChoiceField(label=u'Είδος άσκησης',
-                                choices=EXERCISETYPES)
-
--- main/models.py
+++ main/models.py
@@ -1,29 +1,32 @@
 #-*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms.formsets import formset_factory
 from django.core.validators import MaxValueValidator
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
 
 DIFFICULTIES = (
-        (u'1' , u'Εύκολο'),
-        (u'2' , u'Μέτριο'),
-        (u'3' , u'Δύσκολο'),
+        (u'1', u'Εύκολο'),
+        (u'2', u'Μέτριο'),
+        (u'3', u'Δύσκολο'),
     )
 
 CHOICES = (
-        (u'True' , u'Σωστό'),
-        (u'False' , u'Λάθος'),
+        (u'True', u'Σωστό'),
+        (u'False', u'Λάθος'),
     )
 
 
 class Student(User):
-    level = models.CharField(max_length=1 , choices=DIFFICULTIES)
+    level = models.CharField(max_length=1, choices=DIFFICULTIES)
+
 
 class Example(models.Model):
     chapterId = models.ForeignKey('Chapter')
     question = models.TextField()
     answer = models.TextField()
+
 
 class Chapter(models.Model):
     title = models.CharField(max_length=120)
@@ -31,31 +34,44 @@ class Chapter(models.Model):
     contentMedium = models.TextField()
     contentHard = models.TextField()
 
-#Cant see another way to store each students answers
+
 class Exercise(models.Model):
-    question = models.CharField(max_length=255)
     chapterId = models.ForeignKey('Chapter')
     difficulty = models.CharField(max_length=1, choices=DIFFICULTIES)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    exercise_type = generic.GenericForeignKey('content_type', 'object_id')
 
-class TrueOrFalse(Exercise):
+    def get_type(self):
+        return self.exercise_type.__class__.__name__
+
+
+class TrueOrFalse(models.Model):
+    question = models.CharField(max_length=255)
     answer = models.CharField(max_length=255, choices=CHOICES)
 
-class MultipleChoice(Exercise):
+
+class MultipleChoice(models.Model):
     choice1 = models.CharField(max_length=255)
     choice2 = models.CharField(max_length=255)
     choice3 = models.CharField(max_length=255)
-    choice4 =  models.CharField(max_length=255)
+    choice4 = models.CharField(max_length=255)
+    question = models.CharField(max_length=255)
     answer = models.PositiveIntegerField(validators=[MaxValueValidator(4)])
 
-class FillTheBlanks(Exercise):
+
+class FillTheBlanks(models.Model):
+    question = models.CharField(max_length=255)
     answer = models.CharField(max_length=255)
+
 
 class Test(models.Model):
     chapterId = models.ForeignKey('Chapter')
     difficulty = models.CharField(max_length=1, choices=DIFFICULTIES)
-    ex1 = models.ForeignKey('TrueOrFalse')
-    ex2 = models.ForeignKey('MultipleChoice')
-    ex3 = models.ForeignKey('FillTheBlanks')
+    ex1 = models.ForeignKey('Exercise', related_name='+1')
+    ex2 = models.ForeignKey('Exercise', related_name='+2')
+    ex3 = models.ForeignKey('Exercise', related_name='+3')
+
 
 class Examination(models.Model):
     chapterId = models.ForeignKey('Chapter')
@@ -68,14 +84,15 @@ class Examination(models.Model):
     availableTime = models.PositiveIntegerField()
     difficulty = models.CharField(max_length=1, choices=DIFFICULTIES)
 
+
 class Mistakes(models.Model):
     student = models.ForeignKey('Student')
     exercise = models.ForeignKey('Exercise')
     timeMade = models.DateTimeField()
     answer = models.CharField(max_length=255)
 
+
 class Grade(models.Model):
     student = models.ForeignKey('Student')
     exam = models.ForeignKey('Examination')
-    takenOn = models.DateTimeField() 
-    
+    takenOn = models.DateTimeField()
