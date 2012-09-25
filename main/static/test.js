2f82a811b1373a9ee37d4dfcb99b1b1fdbaae2e3
stauros <kroustou@gmail.com>
2012-09-26 01:15:39 +0300

    handle ajax stuf

--- main/static/test.js
+++ main/static/test.js
@@ -1,23 +1,35 @@
 jQuery(function ($) {
 	"use strict";
 	
-	var exercise_level = $('#exercise-level #id_level');
-	var test_level = $('#test-level #id_level');
-	var exam_level = $('#exam-level #id_level');
+	var exerciseSwitcher = $('#exercise-level select');
+  var exercise_level = $('#exercise-level #id_level');
+  var exercise_chapter = $('#exercise-level #id_chapter');
 
-    exercise_level.on('change', function () {
-        console.log('get exercise');
+
+    exerciseSwitcher.on('change', function () {
         $.ajax({
               type: "POST",
               url: "/get-exercise/",
-              data: { exercise_level: exercise_level.val() }
+              data: { exercise_level: exercise_level.val() , chapter: exercise_chapter.val() }
         }).done(function( msg ) {
             $('.container').html(msg);
         }); 
     });
-	
-	test_level.on('change', function(){
-		console.log('get test');
-		//TODO a lot...
-	});
+
+  var testSwitcher = $('#test-level select');
+	var test_level = $('#test-level #id_level');
+  var test_chapter = $('#test-level #id_chapter');
+  
+  testSwitcher.on('change', function(){
+    $.ajax({
+            type: "POST",
+            url: "/get-test/",
+            data: { test_level: test_level.val() , chapter: test_chapter.val() }
+        }).done(function( msg ) {
+            $('.container').html(msg);
+        }); 
+  });
+  
+
+  // var exam_level = $('#exam-level #id_level');
 });
