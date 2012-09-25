5745af8935b5daf059e7dd8f6d962b8d4ab51017
stauros <kroustou@gmail.com>
2012-09-26 01:17:09 +0300

    change view accordinlgy after using generic relations

--- main/views.py
+++ main/views.py
@@ -3,12 +3,13 @@ from django.views.generic.edit import FormView
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
-from forms import ExampleForm, NewUserForm, SelectLevelForm, TestForm, ExerciseForm, ExamForm, TrueOrFalseForm, FillTheBlanksForm, MultipleChoiceForm, StudentTrueOrFalse, StudentMultipleChoice, StudentFillTheBlanks
+from forms import ExampleForm, NewUserForm, SelectLevelForm, TestForm, ExamForm, TrueOrFalseForm, FillTheBlanksForm, MultipleChoiceForm, StudentTrueOrFalse, StudentMultipleChoice, StudentFillTheBlanks, SelectChapterForm, ExerciseForm
 from django.views.decorators.csrf import csrf_exempt
-from models import Example, Exercise, TrueOrFalse, MultipleChoice, FillTheBlanks, Mistakes
+from models import Example, Exercise, Mistakes, Chapter, Test
 from django.contrib.auth.models import User
 import random
 import utils
+
 
 class RegistrationView(FormView):
     form_class = NewUserForm
@@ -28,15 +29,19 @@ def main(request):
 
 
 def theory(request):
-    return render_to_response('students/theory.html')
+    theory = Chapter.objects.all()
+    return render_to_response('students/theory.html', {'chapters': theory})
+
 
 def example(request):
     examples = Example.objects.all()
-    return  render_to_response('students/example.html',{'examples': examples})
+    return render_to_response('students/example.html', {'examples': examples})
+
 
 @csrf_exempt
 def add(request, type):
     message = ''
+    commit = True
     template_name = 'professor/example.html'
     if type == 'example':
         form = ExampleForm(request.POST or None)
@@ -45,93 +50,96 @@ def add(request, type):
     elif type == 'exam':
         form = ExamForm(request.POST or None)
     elif type == 'exercise':
-        form = ExerciseForm(request.POST or None)
+        commit = False
+        form = ExerciseForm()
+        if request.POST:
+            subform = eval(request.POST['exercise_type'] + 'Form(request.POST)')
+            if subform.is_valid():
+                obj = subform.save(commit=False)
+            form = ExerciseForm(request.POST)
+            if form.is_valid():
+                exercise_object = form.save(commit=False)
+                obj.save()
+                exercise_object.exercise_type = obj
+                exercise_object.save()
         template_name = 'professor/exercise.html'
-    if request.POST and form.is_valid: 
+    if request.POST and form.is_valid and commit:
         form.save()
+    elif not commit:
         message = "Αποθηκεύτηκε επιτυχώς!"
     return render_to_response(template_name, {'form': form, 'type': type, 'message': message})
+
 
 def show_students(request):
     students = User.objects.filter(is_staff=False)
     return render_to_response('professor/students.html', {'students': students})
 
+
 def test(request, test_type="exercise"):
     level = SelectLevelForm()
-    return render_to_response('students/tests.html',{'level': level, 'type': test_type})
+    chapter = SelectChapterForm()
+    return render_to_response('students/tests.html', {'level': level, 'chapter': chapter, 'type': test_type})
+
 
 @csrf_exempt
 def fetch(request):
     exercise_type = request.POST['exercise_type']
+    print exercise_type
     if exercise_type == 'FillTheBlanks':
-        print 'FillTheBlanks'
         form = FillTheBlanksForm()
     elif exercise_type == 'MultipleChoice':
-        form = MultipleChoiceForm() 
+        form = MultipleChoiceForm()
     else:
         form = TrueOrFalseForm()
     return HttpResponse(str(form))
-        
-@csrf_exempt
-def save(request):
-    if request.POST:
-        form_type = request.POST['form_type']
-        if form_type == 'FtB':
-            form = FillTheBlanksForm(request.POST)
-        elif form_type == 'MC':
-            form = MultipleChoiceForm(request.POST) 
-        elif form_type == 'ToF':
-            form = TrueOrFalseForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse_lazy('new-exercise'))
-        else:
-            return HttpResponse(str(form))
-    else:
-            return HttpResponse('Δεν θα έπρεπε να είστε εδώ!')
+
 
 @csrf_exempt
 def get_exercise(request):
     exercise_level = request.POST['exercise_level']
-    type = random.choice([1, 2, 3])
-    type =1
-    if type == 1:
-        exercise_set = FillTheBlanks.objects.filter(difficulty = exercise_level)
-        form = StudentFillTheBlanks()
-    elif type == 2:
-        exercise_set = MultipleChoice.objects.filter(difficulty = exercise_level)
-        form = StudentMultipleChoice()
+    chapter = int(request.POST['chapter']) - 1
+    exercise = Exercise.objects.filter(chapterId=chapter, difficulty=exercise_level)
+    if exercise:
+        exercise = random.choice(list(exercise))
+        return render_to_response('students/exercise.html', {'exercise': exercise})
     else:
-        exercise_set = TrueOrFalse.objects.filter(difficulty = exercise_level)
-        form = StudentTrueOrFalse
-    print exercise_set
-    exercise = random.choice(exercise_set)
-    return render_to_response('students/exercise.html', {'type': type, 'exercise': exercise, 'form': form})
+        return HttpResponse(u'Δεν βρέθηκαν ασκήσεις')
+
 
 @csrf_exempt
 def get_test(request):
-    pass
-    
+    test_level = request.POST['test_level']
+    chapter = int(request.POST['chapter']) - 1
+    test = Test.objects.filter(chapterId=chapter, difficulty=test_level)
+    if test:
+        test = random.choice(list(test))
+        return render_to_response('students/test.html', {'test': test})
+    else:
+        return HttpResponse(u'Δεν βρέθηκαν τέστ')
+
+
 @csrf_exempt
 def get_exam(request):
     pass
- 
+
+
 @csrf_exempt
 def evaluate_exercise(request):
     exercise_pk = request.POST['exercise_pk']
     answer = request.POST['answer']
-    type = request.POST['exercise_type']
+    print answer
     print request.user.pk
-    if(utils.evaluate_answer(exercise_pk=exercise_pk, answer=answer, type=type, student=request.user)):
+    if (utils.evaluate_answer(exercise_pk=exercise_pk, answer=answer, type=type, student=request.user)):
         return HttpResponse('Right')
     else:
         return HttpResponse('Wrong')
-        
+
+
 @csrf_exempt
 def evaluate_test(request):
     pass
 
+
 @csrf_exempt
 def evaluate_exam(request):
     pass
-    
