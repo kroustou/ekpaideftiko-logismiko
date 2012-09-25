15c52f322b2ccee4b1b38b5d66d26d6bb2bee2a3
stauros <kroustou@gmail.com>
2012-09-26 01:16:07 +0300

    change file accordingly to the model changes

--- main/utils.py
+++ main/utils.py
@@ -1,17 +1,13 @@
-from models import Exercise, FillTheBlanks, MultipleChoice, Mistakes, Student
+from models import Exercise, Mistakes, Student
 from datetime import datetime
 
+
 def evaluate_answer(exercise_pk, answer, type, student):
-    if (type == 'ToF'):
-        exercise = TrueOrFalse.objects.get(pk = exercise_pk)
-    elif(type == 'FtB'):
-        exercise = FillTheBlanks.objects.get(pk = exercise_pk)
-    else:
-        exercise = MultipleChoice.objects.get(pk = exercise_pk)
-    if (exercise.answer == answer):
+    exercise = Exercise.objects.get(pk=exercise_pk)
+    if (exercise.exercise_type.answer == answer):
         return True
     else:
-        newMistake = Mistakes(student=student.student , exercise=exercise, 
-                              timeMade=datetime.now(), answer=answer )
+        newMistake = Mistakes(student=student.student, exercise=exercise,
+                              timeMade=datetime.now(), answer=answer)
         newMistake.save()
         return False
